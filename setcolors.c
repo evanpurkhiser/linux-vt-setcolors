a697595dd16a04c06c83f5eb336a8b3ee8830755
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-08-16 09:10:26 -0400

    Better formatting for default color palette

--- setcolors.c
+++ setcolors.c
@@ -20,9 +20,9 @@ static const char *console_paths[] = {
 };
 
 static const char *default_palette[] = {
-"000000","aa0000","00aa00","aa5500","0000aa",
-"aa00aa","00aaaa","aaaaaa","555555","ff5555",
-"55ff55","ffff55","5555ff","ff55ff","55ffff","ffffff" };
+	"000000","aa0000","00aa00","aa5500","0000aa",
+	"aa00aa","00aaaa","aaaaaa","555555","ff5555",
+	"55ff55","ffff55","5555ff","ff55ff","55ffff","ffffff" };
 
 struct palette { unsigned char colors[NUM_COLORS * 16]; };
 
